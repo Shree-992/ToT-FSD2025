e1916a2f32dcdb1ccf61074246029820054a7093
shree-992 <shreya3343@gmail.com>
2025-08-21 11:12:00 +0530

    node js work

---
+++ htmlday2/CSS/JS/index.js
@@ -0,0 +1,30 @@
+const employee={
+    ename: "Rahul Kumar",
+    eage:"30",
+    eaddress:"Ghaziabad",
+    planguage:[
+        {
+        lang1:"java",
+        lang2:"Python"
+    },
+    {
+        database1:"oracle",
+        database2:"MySql"
+    },
+    {
+        framework1:"Hibernate",
+        framework2: "spring"
+    }
+]
+}
+// console.log(employee);
+// console.log(employee.ename);
+// console.log(employee);
+// console.log(employee.planguage[0].lang2);
+employee.company="Infosys";
+console.log(employee);
+
+employee.planguage[1].database1="Mongodb";
+console.log(employee);
+
+
